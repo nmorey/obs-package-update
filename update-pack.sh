b9080ba7718e94cbcf80063cc9cb6a36eb5663a3
Nicolas Morey-Chaisemartin <NMoreyChaisemartin@suse.com>
2017-09-05 18:23:20 +0200

    Add --do-git-version option
    Signed-off-by: Nicolas Morey-Chaisemartin <NMoreyChaisemartin@suse.com>

--- update-pack.sh
+++ update-pack.sh
@@ -12,29 +12,11 @@ PREBUILD_CMDT=""
 DO_COMMIT=0
 DO_SERVICEONLY=0
 TARBALL_NAME=""
+DO_GIT_VERSION_EXTRACT=0
 
 die()
 {
 	echo "$@" >&2
-	exit 1
-}
-
-usage()
-{
-	echo "$0 -n <PACKAGE> -p <OBS_PROJECT> [OPTIONS]"
-	echo " -n,--name            Package name in OBS"
-	echo " -p,--project         Project name in OBS"
-	echo " -T,--tar-name        Tarball name (except version). Defaults to package name"
-	echo " -v,--set-version     Update version in the spec file"
-	echo " -g,--set-git-ver     Update git_ver in the spec file"
-	echo " -R,--do-remove-tar   Remove <PACKAGE>.tar.*"
-	echo " -C,--do-changes      Update the .changes file"
-	echo " -b,--do-build        Build the project after updating"
-	echo " -X,--pre-build=<cmd> Run <cmd> before commiting/build"
-	echo " -c,--do-commit       Commit the project atfer updating"
-	echo " -S,--service-only    Do not access the git but only refresh the files"
-	echo " -x,--verbose         Enable verbose mode"
-	echo " -h,--help            This usage"
 	exit 1
 }
 
@@ -46,6 +28,47 @@ _service_extract_param()
 		sed -e 's/.*<param name="'$param_name'">\(.*\)<\/param>.*/\1/'
 }
 
+_get_git_version()
+{
+	REPLACE_CHAR=''
+	if [ $DO_GIT_VERSION_EXTRACT -ne 0 ]; then
+		REPLACE_CHAR="."
+	fi
+	git describe --abbrev=0 --tags $MATCH_TAG | \
+		sed -e 's/[-:]/'$REPLACE_CHAR'/g' \
+			-e s/$REWRITE_PATTERN/$REWRITE_REPLACEMENT/ -e 's/[^0-9.].*$//'
+}
+_get_git_full_version()
+{
+	REPLACE_CHAR=''
+	if [ $DO_GIT_VERSION_EXTRACT -ne 0 ]; then
+		REPLACE_CHAR="."
+	fi
+	git describe  --tags $MATCH_TAG | \
+		sed -e 's/[-:]/'$REPLACE_CHAR'/g' \
+			-e s/$REWRITE_PATTERN/$REWRITE_REPLACEMENT/
+}
+
+usage()
+{
+	echo "$0 -n <PACKAGE> -p <OBS_PROJECT> [OPTIONS]"
+	echo " -n,--name            Package name in OBS"
+	echo " -p,--project         Project name in OBS"
+	echo " -T,--tar-name        Tarball name (except version). Defaults to package name"
+	echo " -v,--set-version     Update version in the spec file"
+	echo " -g,--set-git-ver     Update git_ver in the spec file"
+	echo " -G,--do-git-version  Extract version from git and update _service (default gets from service results"
+	echo " -R,--do-remove-tar   Remove <PACKAGE>.tar.*"
+	echo " -C,--do-changes      Update the .changes file"
+	echo " -b,--do-build        Build the project after updating"
+	echo " -X,--pre-build=<cmd> Run <cmd> before commiting/build"
+	echo " -c,--do-commit       Commit the project atfer updating"
+	echo " -S,--service-only    Do not access the git but only refresh the files"
+	echo " -x,--verbose         Enable verbose mode"
+	echo " -h,--help            This usage"
+	exit 1
+}
+
 while [ $# -gt 0 ]; do
 	opt="$1"
 	shift
@@ -55,6 +78,7 @@ while [ $# -gt 0 ]; do
 		-T|--tar-name) TARBALL_NAME=$1; shift ;;
 		-v|--set-version) UPDATE_VERSION=1;;
 		-g|--set-git-ver) UPDATE_GITVER=1;;
+		-G|--do-git-version) DO_GIT_VERSION_EXTRACT=1;;
 		-R|--do-remove-tar) DO_REMOVE_TAR=1;;
 		-C|--do-changes) DO_CHANGES=1;;
 		-b|--do-build) DO_BUILD=1;;
@@ -89,6 +113,9 @@ if [ $DO_SERVICEONLY -eq 1 ]; then
    if [ $DO_COMMIT -eq 1 ]; then
 	   die "Cannot run service only and commit"
    fi
+   if [ $DO_GIT_VERSION_EXTRACT -eq 1 ]; then
+	   die "Cannot run service only and extract version from git"
+   fi
 else
 	if [ "$OBS_PROJECT" == "" ]; then
 		die "ERROR: OBS_PROJECT not specified"
@@ -96,42 +123,55 @@ else
 fi
 
 if [ "$TARBALL_NAME" == "" ]; then
-	TARBALL_NAME=PACKAGE
+	TARBALL_NAME=$PACKAGE
 fi
+
+# Try to fetch the rewrite patterns from the _service file
+export REWRITE_PATTERN=$(_service_extract_param versionrewrite-pattern \
+												$OBS_PROJECT/$PACKAGE/_service |\
+							 sed -e 's/(/\\(/' -e 's/)/\\)/')
+export REWRITE_REPLACEMENT=$(_service_extract_param versionrewrite-replacement \
+													$OBS_PROJECT/$PACKAGE/_service)
+if [ "$REWRITE_PATTERN" == "" ]; then
+	#If not set, use a generic rule
+	REWRITE_PATTERN='\(.*\)'
+	REWRITE_REPLACEMENT='\1'
+fi
+
+export MATCH_TAG=$(_service_extract_param match-tag $OBS_PROJECT/$PACKAGE/_service)
+if [ "$MATCH_TAG" != "" ]; then
+	MATCH_TAG="--match $MATCH_TAG"
+fi
+
 if [ $DO_SERVICEONLY -ne 1 ]; then
+	# Get version from git
 	rm -Rf "$OBS_PROJECT/$PACKAGE"
 	osc co $OBS_PROJECT $PACKAGE
+
+	VERSION=$(_get_git_version)
 	NEW_SHA=$(git rev-parse HEAD)
-
-	# Try to fetch the rewrite patterns from the _service file
-	REWRITE_PATTERN=$(_service_extract_param versionrewrite-pattern \
-											 $OBS_PROJECT/$PACKAGE/_service |\
-				   sed -e 's/(/\\(/' -e 's/)/\\)/')
-	REWRITE_REPLACEMENT=$(_service_extract_param versionrewrite-replacement \
-						  $OBS_PROJECT/$PACKAGE/_service)
-	if [ "$REWRITE_PATTERN" == "" ]; then
-	   #If not set, use a generic rule
-	   REWRITE_PATTERN='\(.*\)'
-	   REWRITE_REPLACEMENT='\1'
-	fi
-
-	MATCH_TAG=$(_service_extract_param match-tag \
-									   $OBS_PROJECT/$PACKAGE/_service)
-	if [ "$MATCH_TAG" != "" ]; then
-		MATCH_TAG="--match $MATCH_TAG"
-	fi
-	VERSION=$(git describe --abbrev=0 --tags $MATCH_TAG | \
-				  sed -e 's/[-:]//g' -e s/$REWRITE_PATTERN/$REWRITE_REPLACEMENT/ -e 's/-.*$//')
 	OLD_SHA=$(grep revision "$OBS_PROJECT/$PACKAGE/_service"  | \
 				  sed -e 's/^.*">\([0-9a-f]\{40\}\).*$/\1/')
 	CHANGES=$(echo "Auto update from $OLD_SHA to $NEW_SHA";\
 			  git log HEAD ^$OLD_SHA  --no-merges --format="  * %s")
 
 	# Update service file with the proper revission
-	sed -i -e 's/\(<param name="revision">\)\([0-9a-f]\{40\}\)\(<\/param>\)/\1'$NEW_SHA'\3/' "$OBS_PROJECT/$PACKAGE/_service"
+	sed -i -e 's/\(<param name="revision">\)\([0-9a-f]\{40\}\)\(<\/param>\)/\1'$NEW_SHA'\3/'\
+		"$OBS_PROJECT/$PACKAGE/_service"
 
+
+	if [ $DO_GIT_VERSION_EXTRACT -ne 0 ]; then
+		# We need to bump the version name of the package
+		# Probably to avoid the service screwing up weird tags with - in them
+		FULL_VERSION=$(_get_git_full_version)
+		# Update version in the service file
+		sed -i -e 's/\(<param name="version">\).*\(<\/param>\)/\1'$FULL_VERSION'\2/' \
+			"$OBS_PROJECT/$PACKAGE/_service"
+
+	fi
 	cd "$OBS_PROJECT/$PACKAGE/"
 else
+	#Get version from spec and suffix later on from _service result
 	VERSION=$(rpmspec -P $PACKAGE.spec  | grep Version: | sed -e  's/\(Version:[[:space:]]*\)//')
 fi
 
@@ -144,6 +184,9 @@ LOG=$(osc service disabledrun)
 # Get git suffix. This allows custom naming to work
 GIT_SUFF=$(echo "$LOG" | egrep '\.tar\.(gz|xz|bz2)' | head -n 1 | \
 			   sed -e 's/.*\('$TARBALL_NAME'[^ ]*\)\.tar\.\(gz\|xz\|bz2\).*/\1/' -e 's/'$TARBALL_NAME'-'$VERSION'//')
+if [ "$GIT_SUFF" == "" ]; then
+	GIT_SUFF="%{nil}"
+fi
 
 if [ $UPDATE_VERSION -eq 1 ]; then
 	sed -i -e 's/\(Version:[[:space:]]*\)[0-9].*/\1'$VERSION'/' $PACKAGE.spec
